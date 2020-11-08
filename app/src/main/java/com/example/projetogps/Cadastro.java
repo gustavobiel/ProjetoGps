41fb52fd3c58d333975542dc9990a87dade1cac9
gustavobiel <56567873+gustavobiel@users.noreply.github.com>
2020-11-08 00:48:55 -0300

    cadastro de locais

---
+++ app/src/main/java/com/example/projetogps/Cadastro.java
@@ -0,0 +1,18 @@
+package com.example.projetogps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Cadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+
+    public void salvar(View view) {
+    }
+}
