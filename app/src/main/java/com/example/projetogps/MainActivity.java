b4404805b163ab5a6fcab901a187becde3437bf6
Ranieri <57837805+xRanierix@users.noreply.github.com>
2020-11-08 13:12:00 -0300

    Concluindo Ativação do GPS e Adicionando onStop

--- app/src/main/java/com/example/projetogps/MainActivity.java
+++ app/src/main/java/com/example/projetogps/MainActivity.java
@@ -12,11 +12,13 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private LocationManager locationManager;
     private LocationListener locationListener;
+    private static final int GPS_REQUEST_PERMISSION_CODE = 1001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,24 +34,51 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-            if(ActivityCompat.checkSelfPermission
-                    (this.Manifest.permission.ACESS_FILE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                locationManager.requestLocationUpdates(
-                        LocationManager.GPS_PROVIDER,
-                        5000, 5,
-                        locationListener
-                );
-            }
-        else{
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    5000,
+                    10,
+                    locationListener
+            );
+        } else {
             ActivityCompat.requestPermissions
                     (this,
-                    new String [] {Manifest.permission.ACCESS_FINE_LOCATION}
-                    1043
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION}
+                            GPS_REQUEST_PERMISSION_CODE
                     );
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == GPS_REQUEST_PERMISSION_CODE) {
+            if (grantResults.length > && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    locationManager.requestLocationUpdates(
+                            LocationManager.GPS_PROVIDER,
+                            5000,
+                            10,
+                            locationListener
+                    );
+                }
+            } else {
+                Toast.makeText(this, "Sem GPS!!!", Toast.LENGTH_SHORT).show();
             }
+        }
 
     }
 
-    public void novoLugar(View view) {
+    @Override
+    protected void onStop() {
+        super.onStop();
+        locationManager.removeUpdates(locationListener);
     }
+
 }
+
+    //public void novoLugar(View view) {
+    //}
